5f5bb6a8e6c1be5964d1a528f018070cc198cea3
Axel Han <axknightroad@gmail.com>
2016-01-04 14:12:12 +0800

    add
    add

---
+++ elpa/python-environment-20140321.1116/python-environment-autoloads.el
@@ -0,0 +1,18 @@
+;;; python-environment-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("python-environment-pkg.el" "python-environment.el"
+;;;;;;  "test-python-environment.el") (21446 16367 643353 0))
+
+;;;***
+
+(provide 'python-environment-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; python-environment-autoloads.el ends here
